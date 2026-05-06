d905ea73add0616e639a2aa75d28a602d68dee80
Your Name <you@example.com>
2026-05-07 01:06:28 +0300

    query clip stuff :D

--- Audio match.py
+++ Audio match.py
@@ -1,6 +1,7 @@
 from scipy.io import wavfile
 import numpy as np
 import matplotlib.pyplot as plt
+from sympy import fourier_series, pi, plot
 
 sample_rate, song = wavfile.read("Song.wav")
 
@@ -16,19 +17,42 @@ total_samples = len(song)
 
 time = np.arange(total_samples)/sample_rate
 
+short_time = 4
 
-short_time = 10
 samples_to_plot = int(short_time*sample_rate)
+
 
 
 x_time = time[0:samples_to_plot]
 y_song = mono_song[0:samples_to_plot]
 
+plt.subplot(2,1,1)
 plt.plot(x_time,y_song)
-plt.title("Audio Signal in Time Domain (First 50ms)")
+plt.title("Audio Signal in Time Domain")
+plt.xlabel("Time (seconds)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+
+
+
+
+duration_t=15
+start_t= 167
+end_t = start_t + duration_t
+query_start = int(start_t*sample_rate)
+query_end = int(end_t*sample_rate)
+xq_time = time[query_start:query_end]
+y_query = mono_song[query_start:query_end]
+
+
+plt.subplot(2,1,2)
+plt.plot(xq_time,y_query)
+plt.title("Query in time domain")
 plt.xlabel("Time (seconds)")
 plt.ylabel("Amplitude")
 plt.grid(True)
 plt.show()
 
- 
+
+def fourier_transform(array)
+
